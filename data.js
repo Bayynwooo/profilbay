97629f541d584942250b37d81367e180a69aab34
Bayynwooo <np007913@gmail.com>
2026-06-20 17:04:06 +0700

    Add files via upload
    portofolio terbaru syaa

---
+++ data.js
@@ -0,0 +1,21 @@
+const databaseProyek = {
+    "1": {
+        judul: "Web Developer",
+        keahlian: ['Mampu membuat dan mengembangkan website dinamis berbasis PHP dan MySQL dengan tampilan responsif.'],
+        gambar: "https://www.rackh.com/wp-content/uploads/2024/07/4-Bahasa-Pemrograman-untuk-Membuat-Website-Terpopuler.jpg",
+        latar: "Website portofolio ini dibuat sebagai media untuk menampilkan profil, keterampilan, dan proyek yang telah saya kerjakan dalam bidang pengembangan web.",
+        tantangan: "Terus belajar teknologi baru untuk menghadapi perkembangan dunia web.",
+        pelajaran: "Mempelajari pengembangan website mulai dari frontend hingga backend menggunakan HTML, CSS, JavaScript, PHP, dan MySQL.",
+        link: ""
+    },
+
+     "2": {
+        judul: "Desainer Grafis",
+        keahlian: ['Mampu membuat desain visual yang kreatif, menarik, dan komunikatif untuk berbagai kebutuhan digital.'],
+        gambar: "https://idplus.id/wp-content/uploads/2021/09/graphic-design-header-3.jpg",
+        latar: "Proyek ini dibuat untuk menampilkan kemampuan desain grafis dalam menghasilkan karya visual yang kreatif dan menarik.",
+        tantangan: "Mengubah ide menjadi karya visual yang menarik dan mudah dipahami.",
+        pelajaran: "Mempelajari prinsip desain, pemilihan warna, tipografi, komposisi visual, serta penggunaan aplikasi desain untuk menghasilkan karya yang menarik dan komunikatif.",
+        link: ""
+    },
+};
